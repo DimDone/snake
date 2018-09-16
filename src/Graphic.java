3d52993c5297fc78ac2b1c583b75a8a9e96d8a49
REAL-BS\student12 <kitaev-mitynka@mail.ru>
2018-09-16 12:17:58 +0400

    Create graphic

---
+++ src/Graphic.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Graphic extends JPanel {
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+            // TODO: - draw sprites
+        g.fillRect(0,0,800,600);
+    }
+}
+
